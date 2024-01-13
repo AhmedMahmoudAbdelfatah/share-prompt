3990b37a65e779a6bc938f7c38f8c1b67bb8758d
Ahmed Mahmoud <am4813@fayoum.edu.eg>
2024-01-13 13:34:09 +0200

    update

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,8 +1,9 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import { NextRequest } from "next/server";
 
-export const dynamic = 'force-dynamic';
-export const GET = async () => {
+// export const dynamic = 'force-dynamic';
+export const GET = async (request: NextRequest) => {
   try {
     await connectToDB();
     const posts = await Prompt.find({}).populate('creator');
